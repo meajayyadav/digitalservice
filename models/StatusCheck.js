0695487fd11a5b2c5048f2c99263af1ce9a397aa
ajayyadav-klps <ajay.yadav@kalpas.in>
2025-10-30 17:26:50 +0530

    Updated React UI

--- models/StatusCheck.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const statusSchema = new mongoose.Schema({
-  id: String,
-  client_name: String,
-  timestamp: String
-});
-
-export default mongoose.model("StatusCheck", statusSchema);
